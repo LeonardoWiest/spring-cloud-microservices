6bc991e5317b9da4e8ca3c460129abca07561c92
Leonardo Wiest <leonardowiest@hotmail.com>
2019-11-16 21:30:43 -0200

    Criado projeto e implementada a primeira API

---
+++ loja/src/main/java/github/com/leonardowiest/loja/dto/EnderecoDTO.java
@@ -0,0 +1,35 @@
+package github.com.leonardowiest.loja.dto;
+
+public class EnderecoDTO {
+
+    private String rua;
+
+    private String numero;
+
+    private String uf;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+}

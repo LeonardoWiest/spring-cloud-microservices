04bb01dae1ccafc357b4c05c402c2be5b530f051
Leonardo Wiest <leonardowiest@hotmail.com>
2019-12-01 17:34:20 -0200

    Update

--- loja/src/main/java/github/com/leonardowiest/loja/service/CompraService.java
+++ loja/src/main/java/github/com/leonardowiest/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package github.com.leonardowiest.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,17 @@ public class CompraService {
     @Autowired
     private RestTemplate client;
 
+    @Autowired
+    private DiscoveryClient euDiscoveryClient;
+
     public void processarCompra(CompraDTO compraDTO) {
 
         ResponseEntity<InfoFornecedorDTO> fornecedorDTO = client.exchange("http://fornecedor/info/" + compraDTO.getEndereco().getUf(), HttpMethod.GET,
                 null, InfoFornecedorDTO.class);
+
+        euDiscoveryClient.getInstances("fornecedor").stream().forEach(instanciaFornecedor -> {
+            System.out.println("localhost:" + instanciaFornecedor.getPort());
+        });
 
         System.out.println(fornecedorDTO.getBody().getEndereco());
     }
